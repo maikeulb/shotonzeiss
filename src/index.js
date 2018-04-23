10ac168193627cc48fe4a497b4c8f78f41621e2b
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-23 19:35:43 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import * as actions from './store/actions/index';
+import photosReducer from './store/reducers/photos';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -10,10 +16,16 @@ import './index.css';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
+const store = createStore(
+  compose(applyMiddleware(thunk))
+);
+
 const app = (
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
