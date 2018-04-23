d6cf1917d24b9cf4e884deddbf0d67086301b268
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-22 22:22:48 -0400

    add photos container to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+
+import Photos from './containers/Photos';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path="/" component={PhotosContainer} />
+       </Switch>
       </div>
     );
   }
