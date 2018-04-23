3aae79b80bf2702829ce0605392f68a0d40d0f35
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-22 22:25:12 -0400

    wire up index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './index.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+const app = (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
