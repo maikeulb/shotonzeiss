058ed50368ad66b1f990d9d479b4ac6bc2060c0b
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-23 20:03:25 -0400

    add reducers

--- src/store/reducers/photos.py
+++ src/store/reducers/photos.py
@@ -1,22 +1,35 @@
-import merge from 'lodash/merge';
+import * as actionTypes from '../actions/actionTypes'
+import {updateObject} from '../utility'
 
-import {
-  RECEIVE_SINGLE_PHOTO,
-  RECEIVE_ALL_PHOTOS
-} from '../actions/photo_actions';
+const initialState = {}
 
-const defaultState = {};
+const addPhoto = (state, action) = > {
+    const updatedPhoto = {[action.photoName]: state.photos[action.photoName] + 1}
+    const updatedPhotos = updateObject(state.photos, updatedPhoto);
+    const updatedState = {
+        photos: updatedPhotos,
+    }
+    return updateObject(state, updatedState); };
 
-const PhotosReducer = (state = defaultState, action) => {
-  Object.freeze(state);
-  switch(action.type) {
-    case RECEIVE_SINGLE_PHOTO:
-      return merge({}, state, action.photo);
-    case RECEIVE_ALL_PHOTOS:
-      return merge({}, state, action.photos);
-    default:
-      return state;
-  }
+const removePhoto = (state, action) = > {
+    const updatedPhos = {[action.photoName]: state.photos[action.photoName] - 1}
+    const updatedPho = updateObject(state.photos, updatedPho);
+    const updatedSt = {
+        photos: updatedPho,
+    }
+    return updateObject(state, updatedSt); };
+
+const fetchPhotosFailed = (state, action) = > {
+    return updateObject(state, {error: true}); };
+
+const reducer = (state=initialState, action) = > {
+    switch(action.type) {
+        case actionTypes.ADD_PHOTO: return addPhoto(state, action);
+        case actionTypes.REMOVE_PHOTO: return removePhoto(state, action);
+        case actionTypes.FETCH_ALL_PHOTOS: return fetchAllPhotos(state, action);
+        case actionTypes.FETCH_SINGLE_PHOTO: return fetchSinglePhoto(state, action);
+        case actionTypes.FETCH_PHOTOS_FAILED: return fetchPhotosFailed(state, action);
+        default: return state; }
 };
 
-export default PhotosReducer;
+export default reducer
