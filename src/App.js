d1957d77a49dcb50c6d962b5abd09cde210d449b
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-24 16:32:08 -0400

    add wire up authentication and login/logout components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout';
 import Photos from './containers/Photos/Photos';
+import Login from './containers/Login/Login';
+import Logout from './containers/Logout/Logout';
 import './App.css';
+
+// class App extends Component {
+//   render () {
+//     let routes = (
+//         <Route path="/" exact component={Photos} />
+//     );
+//     return (
+//       <div className="App">
+//         <Layout>
+//           {routes}
+//         </Layout>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 class App extends Component {
   render () {
     let routes = (
+      <Switch>
+        <Route path="/login" component={Login} />
         <Route path="/" exact component={Photos} />
+        <Redirect to="/" />
+      </Switch>
     );
+
+    if ( this.props.isAuthenticated ) {
+      routes = (
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <div className="App">
         <Layout>
@@ -20,5 +56,10 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.user !== null
+  };
+};
 
-export default App;
+export default withRouter( connect( mapStateToProps )( App ) );
--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import * as actions from './store/actions/index';
 import photosReducer from './store/reducers/photos';
+import authReducer from './store/reducers/auth';
 
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 
 const rootReducer = combineReducers({
-    photos: photosReducer
+  photos: photosReducer,
+  auth: authReducer
 });
 
 const store = createStore(
   rootReducer, 
   compose(applyMiddleware(thunk))
 );
+
+store.dispatch(actions.verifyAuth());
 
 const app = (
   <Provider store={store}>
