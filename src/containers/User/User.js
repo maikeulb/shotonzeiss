4e9cc4a5171cfebc5e220c330d85809afde76e83
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-24 21:06:26 -0400

    add User container

---
+++ src/containers/User/User.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+
+import withErrorHandler from '../../hoc/withErrorHandler';
+
+import Photos from './Photos/Photos';
+
+import axios from '../../axios-photos';
+import * as actions from '../../store/actions/index';
+import styled from 'styled-components';
+
+class User extends Component {
+  state = {
+    type: "photos",
+  };
+
+  componentDidMount() {
+    // this.props.fetchUserProfile(this.props.match.params.id);
+    // this.props.fetchUserPhotos(this.props.match.params.id);
+  }
+
+  render() {
+    let { userId, username } = this.props.user;
+
+    let personalButton = <Upload />;
+
+    return (
+      <div>
+        username
+      </div>
+      <div>
+        <Photos userId={userId}/>
+      </div>
+    );
+  }
+
+}
+const mapStateToProps = state => {
+  return {
+    user: state.user.user,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onFetchUserProfile: (userId) => dispatch(actions.fetchUserProfile(userId)),
+  onFetchUserPhotos: (userId) => dispatch(actions.fetchUserPhotos(userId)),
+});
+
+export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( User, axios ) );
