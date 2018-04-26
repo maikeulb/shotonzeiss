da3334e2d75a9560167976c496c254cb340493e9
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-25 21:06:46 -0400

    remove dead code

--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -22,7 +22,6 @@ class User extends Component {
 
   render() {
     let profile = <Spin />;
-    // let { userId, username } = this.props.profile;
     if ( !this.props.loading && this.props.profile[0]) {
       profile = (
         <div>
@@ -34,41 +33,6 @@ class User extends Component {
     return profile
   }
 }
-
-// let propsUser = { 
-//   userId: 1, 
-//   username: "mike@gmail.com"
-// };
-
-// let propsPhotos = [
-//   { id: 9, 
-//     photoUrl: "https://c2.staticflickr.com/4/3261/3228647240_ff320e465f_z.jpg",
-//     username: "michael@gmail.com",
-//     userId: 1
-//   },
-//   { id: 10, 
-//     photoUrl: "https://c2.staticflickr.com/4/3851/14586984988_be404b8229.jpg", 
-//     username: "michael@gmail.com",
-//     userId: 1
-//   },
-//   { id: 11, 
-//     photoUrl: "https://c2.staticflickr.com/2/1008/5187060734_fe0f56630f_z.jpg",
-//     username: "michael@gmail.com",
-//     userId: 1
-//   },
-//   { id: 12, 
-//     photoUrl: "https://c1.staticflickr.com/5/4131/5099175981_d81d3ced3a_z.jpg",
-//     username: "michael@gmail.com",
-//     userId: 1
-//   },
-// ];
-
-// const mapStateToProps = state => {
-//   return {
-//     user: propsUser,
-//     photos: propsPhotos,
-//   };
-// };
 
 const mapStateToProps = state => {
   return {
